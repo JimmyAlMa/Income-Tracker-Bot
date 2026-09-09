345a829c0de531a1b916a0f8357406995def91e9
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-09-09 18:53:48 +0700

    feat: add save_sheet to append expense data to Google Sheet

--- main.py
+++ main.py
@@ -78,3 +78,12 @@ def get_sheet():
         creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=scopes)
     client = gspread.authorize(creds)
     return client.open(GOOGLE_SHEET_NAME).worksheet("Sheet2")
+
+def save_sheet(data: dict):
+    sheet = get_sheet()
+    sheet.append_row([
+        datetime.now().strftime("%Y-%m-%d %H:%M"),
+        data.get("kategori", ""),
+        data.get("nominal", 0),
+        data.get("deskripsi", "")
+    ])
